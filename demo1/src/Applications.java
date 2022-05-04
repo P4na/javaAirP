2940c97f9292162e01b830225624d2eabb2ec0d1
“pana” <vip_P4na@protonmail.com>
2022-05-04 12:48:05 +0200

    sistemare classi per Airport

---
+++ demo1/src/Applications.java
@@ -0,0 +1,19 @@
+public class Applications {
+    private int pk;
+    private String nome;
+    private String cognome;
+    private Aereo volo;
+
+    public Applications(int pk){
+        this.volo = new Autonov();
+    }
+}
+
+
+/*
+To-do:
+1 analisi Entity
+2 design entity creiamo le classi
+3 attributi delle entity che vengono fuori dalla analisi
+4 relazione sulla entity
+*/
---
+++ demo1/src/Autonov.java
@@ -0,0 +1,3 @@
+public class Autonov extends Aereo{
+
+}
